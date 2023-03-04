0b5ab6ea7fef5e1dae589fca3f7959b44bc6a77c
Roman <romanshliakhov@gmail.com>
2023-03-04 18:27:38 +0200

    start area page

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,7 +1,7 @@
 // import './components/sliders.js';
 // import './components/select.js';
 import './components/burger.js';
-// import './components/accordion.js';
+import './components/accordion.js';
 // import './components/modals.js';
 import './components/replaceEl.js';
 // import './components/phoneMask.js';
