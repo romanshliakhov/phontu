0f3dd00a3b760f10210b12859ef50e90cb294a01
Roman <romanshliakhov@gmail.com>
2023-03-05 00:33:22 +0200

    add adaptive

--- source/js/components/replaceEl.js
+++ source/js/components/replaceEl.js
@@ -9,33 +9,9 @@ let replaceSettings = {
 }
 
 let elementName = {
-  headerLogo: '.header__logo',
-  headerLogoParent: '.header__inner',
-  headerLogoMobile: '.mobile__menu-head',
-
-  burger: '.burger',
-  burgerDefault: '.header__inner',
-  burgerMobile: '.mobile__menu-head',
-
-  basket: '.basket',
-  basketDefault: '.header__icons',
-  basketMobile: '.mobile__menu-head',
-
-  lang: '.lang',
-  langParent: '.header__logo',
-  langMobile: '.mobile__profile',
-
-  heroSliderControls: '.hero__slider-controls',
-  heroSliderControlsParent: '.hero__inner',
-  heroSliderControlsMobile: '.hero__slider',
-
-  visionaryBanner: '.visionary__banner',
-  visionaryBannerParent: '.visionary__info',
-  visionaryBannerMobile: '.visionary__info-bot',
-
-  shopFilters: '.shop__aside',
-  shopFiltersParent: '.shop__wrapp',
-  shopFiltersMobile: '.shop__info-head',
+  headerNav: '.header__nav',
+  headerNavParent: '.mobile__menu',
+  headerNavMobile: '.header__logo',
 }
 
 let items = [...document.querySelectorAll('.wrapper')];
@@ -58,64 +34,10 @@ const replaceElements = (elements, elementClass, desktopClass, mobileClass, mobi
   });
 }
 
-burgerBtn = document.querySelector('.burger');
-
 window.addEventListener('resize', () => {
-  if(burgerBtn) {
-    burgerBtn.addEventListener('click', function () {
-      if(burgerBtn.classList.contains('active')) {
-        console.log(burgerBtn);
-
-        replaceElements(items, elementName.headerLogo, elementName.headerLogoParent, elementName.headerLogoMobile, replaceSettings.afterbegin, replaceSettings.afterbegin, replaceSettings.smallTablet);
-
-        replaceElements(items, elementName.burger, elementName.burgerDefault, elementName.burgerMobile, replaceSettings.afterbegin, replaceSettings.afterbegin, replaceSettings.smallTablet);
-
-        replaceElements(items, elementName.basket, elementName.basketDefault, elementName.basketMobile, replaceSettings.beforeend, replaceSettings.beforeend, replaceSettings.smallTablet);
-      } else {
-        replaceElements(items, elementName.headerLogo, elementName.headerLogoMobile, elementName.headerLogoParent, replaceSettings.afterbegin, replaceSettings.afterbegin, replaceSettings.smallTablet);
-
-        replaceElements(items, elementName.burger, elementName.burgerMobile, elementName.burgerDefault, replaceSettings.afterbegin, replaceSettings.afterbegin, replaceSettings.smallTablet);
-
-        replaceElements(items, elementName.basket, elementName.basketMobile, elementName.basketDefault, replaceSettings.beforeend, replaceSettings.beforeend, replaceSettings.smallTablet);
-      }
-    })
-  }
-
-  replaceElements(items, elementName.lang, elementName.langParent, elementName.langMobile, replaceSettings.afterend, replaceSettings.afterend, replaceSettings.smallTablet);
-
-  replaceElements(items, elementName.heroSliderControls, elementName.heroSliderControlsParent, elementName.heroSliderControlsMobile, replaceSettings.afterbegin, replaceSettings.afterbegin, replaceSettings.tablet);
-
-  replaceElements(items, elementName.visionaryBanner, elementName.visionaryBannerParent, elementName.visionaryBannerMobile, replaceSettings.beforebegin, replaceSettings.beforebegin, replaceSettings.tablet);
-
-  replaceElements(items, elementName.shopFilters, elementName.shopFiltersParent, elementName.shopFiltersMobile, replaceSettings.afterend, replaceSettings.afterbegin, replaceSettings.tablet);
+  replaceElements(items, elementName.headerNav, elementName.headerNavMobile, elementName.headerNavParent, replaceSettings.afterbegin, replaceSettings.afterend,  replaceSettings.smallTablet);
 });
 
 window.addEventListener('DOMContentLoaded', () => {
-  if(burgerBtn) {
-    burgerBtn.addEventListener('click', function () {
-      if(burgerBtn.classList.contains('active')) {
-        console.log(burgerBtn);
-
-        replaceElements(items, elementName.headerLogo, elementName.headerLogoParent, elementName.headerLogoMobile, replaceSettings.afterbegin, replaceSettings.afterbegin, replaceSettings.smallTablet);
-
-        replaceElements(items, elementName.burger, elementName.burgerDefault, elementName.burgerMobile, replaceSettings.afterbegin, replaceSettings.afterbegin, replaceSettings.smallTablet);
-
-        replaceElements(items, elementName.basket, elementName.basketDefault, elementName.basketMobile, replaceSettings.beforeend, replaceSettings.beforeend, replaceSettings.smallTablet);
-      } else {
-        replaceElements(items, elementName.headerLogo, elementName.headerLogoMobile, elementName.headerLogoParent, replaceSettings.afterbegin, replaceSettings.afterbegin, replaceSettings.smallTablet);
-
-        replaceElements(items, elementName.burger, elementName.burgerMobile, elementName.burgerDefault, replaceSettings.afterbegin, replaceSettings.afterbegin, replaceSettings.smallTablet);
-
-        replaceElements(items, elementName.basket, elementName.basketMobile, elementName.basketDefault, replaceSettings.beforeend, replaceSettings.beforeend, replaceSettings.smallTablet);
-      }
-    })
-  }
-
-  replaceElements(items, elementName.lang, elementName.langParent, elementName.langMobile, replaceSettings.afterend, replaceSettings.afterend, replaceSettings.smallTablet);
-
-  replaceElements(items, elementName.heroSliderControls, elementName.heroSliderControlsParent, elementName.heroSliderControlsMobile, replaceSettings.afterbegin, replaceSettings.afterbegin, replaceSettings.tablet);
-
-  replaceElements(items, elementName.visionaryBanner, elementName.visionaryBannerParent, elementName.visionaryBannerMobile, replaceSettings.beforebegin, replaceSettings.beforebegin, replaceSettings.tablet);
-
-  replaceElements(items, elementName.shopFilters, elementName.shopFiltersParent, elementName.shopFiltersMobile, replaceSettings.afterend, replaceSettings.afterbegin, replaceSettings.tablet);
+  replaceElements(items, elementName.headerNav, elementName.headerNavMobile, elementName.headerNavParent, replaceSettings.afterbegin, replaceSettings.afterend,  replaceSettings.smallTablet);
 });
