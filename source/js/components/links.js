bf54cbe5fd2f9f6d86faf03b171d79a2fa173cb1
Roman <romanshliakhov@gmail.com>
2023-03-13 17:30:57 +0200

    add links script

--- source/js/components/links.js
+++ source/js/components/links.js
@@ -1,17 +1,16 @@
 const links = [...document.querySelectorAll('.tags__btn')];
+const linkMore = document.querySelector('.tags__more');
 
 links.forEach(function(link, i){
   if(i > 4) {
     link.classList.add('hide');
+
+    linkMore.addEventListener('click', () => {
+      link.classList.remove('hide');
+      linkMore.style.opacity="0";
+      linkMore.style.visibility="hidden";
+    })
+  } else if (i < 4) {
+    // linkMore.style.display="none";
   }
-
-  linkMore = document.querySelector('.tags__more');
-
-  linkMore.addEventListener('click', () => {
-    link.classList.remove('hide');
-    linkMore.style.display="none";
-  })
-
 })
-
-console.log(links);
