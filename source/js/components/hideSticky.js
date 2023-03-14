69c3e1cfa5545d3d768e86a5b9ed68c0c681b994
Roman <romanshliakhov@gmail.com>
2023-03-14 11:51:34 +0200

    start fix scroll

--- source/js/components/hideSticky.js
+++ source/js/components/hideSticky.js
@@ -1,14 +1,12 @@
-const breakpoint = 991;
-
 const stickyEl = document.querySelector('.sticky__links');
 
-const addCustomClass = function(el, customClass) {
-  el.classList.add(`${customClass}`);
-}
+// const addCustomClass = function(el, customClass) {
+//   el.classList.add(`${customClass}`);
+// }
 
 window.onscroll = function () {
   let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrolled > 3000) {
+  if (scrolled > 1500) {
     stickyEl.classList.add('hide');
   } else {
     stickyEl.classList.remove('hide');
@@ -18,19 +16,4 @@ window.onscroll = function () {
 
 
 
-const desktopeInit = () => {
-  let containerWidth = document.documentElement.clientWidth;
 
-  if (containerWidth >= breakpoint) {
-
-  }
-};
-
-
-window.addEventListener("DOMContentLoaded", () => {
-  desktopeInit();
-});
-
-window.addEventListener("resize", () => {
-  desktopeInit();
-});
